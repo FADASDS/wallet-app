198cdd098970329c99aac2ca139843d5fc77b60d
FADASDS <103619943+FADASDS@users.noreply.github.com>
2025-02-24 01:41:53 +0700

    Add transaction.

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/lib/pq"
 	"os"
+	"time"
 	"wallet-app/internal/dto"
 )
 
@@ -33,6 +34,36 @@ func NewStorage() (*storePG, error) {
 }
 
 func (s *storePG) Send(from string, to string, amount float64) error {
+	tx, err := s.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+
+	_, err = tx.Exec("UPDATE wallet_tbl SET balance=balance-$1 WHERE id=$2", amount, from)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec("UPDATE wallet_tbl SET balance=balance+$1 WHERE id=$2", amount, to)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec("INSERT INTO transactions_tbl (from_wallet, to_wallet, amount, date) VALUES ($1, $2, $3, $4)", from, to, amount, time.Now())
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -40,7 +71,7 @@ func (s *storePG) Send(from string, to string, amount float64) error {
 func (s *storePG) GetLastNTransactions(n int64) ([]dto.TransactionDTO, error) {
 
 	var transaction []dto.TransactionDTO
-	rows, err := s.db.Query("SELECT * FROM wallet_db ORDER BY  crtn_date LIMIT $1", n)
+	rows, err := s.db.Query("SELECT * FROM wallet_tbl ORDER BY crtn_date LIMIT $1", n)
 
 	if err != nil {
 		return nil, err
